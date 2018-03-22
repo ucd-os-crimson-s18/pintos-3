0d4b020d84506e780db29cf8ba940454412648ae
Pintos Developer <pintos@pintos.com>
2018-03-22 12:39:34 -0600

    Progress on syscall_handler

--- src/userprog/process.c
+++ src/userprog/process.c
@@ -92,7 +92,7 @@ start_process (void *file_name_)
    This function will be implemented in problem 2-2.  For now, it
    does nothing. */
 int
-process_wait (tid_t child_tid UNUSED) 
+process_wait (tid_t child_tid) 
 {
   //return -1;
   // Temporary fix to process wait
